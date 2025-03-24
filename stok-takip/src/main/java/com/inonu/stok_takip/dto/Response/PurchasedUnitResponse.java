0e59d3723b4f8af182e531d752b5e60db1be28af
EXCALIBUR <reisyusuf.1903@gmail.com>
2025-03-24 16:14:44 +0300

    update

--- stok-takip/src/main/java/com/inonu/stok_takip/dto/Response/PurchasedUnitResponse.java
+++ stok-takip/src/main/java/com/inonu/stok_takip/dto/Response/PurchasedUnitResponse.java
@@ -9,6 +9,17 @@ public class PurchasedUnitResponse {
         this.name = name;
     }
 
+    public PurchasedUnitResponse() {
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public Long getId() { return id; }
     public String getName() { return name; }
 }
