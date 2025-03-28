adf5d8988e7f64c7c68bfe4f88a7890d736051ab
oguzhantimur4444 <oguzhantimur4444@gmail.com>
2025-03-28 11:54:13 +0300

    Refactor request DTOs: update fields and comments for clarity and relevance

--- stok-takip/src/main/java/com/inonu/stok_takip/dto/Request/TicketSalesDetailCreateRequest.java
+++ stok-takip/src/main/java/com/inonu/stok_takip/dto/Request/TicketSalesDetailCreateRequest.java
@@ -3,6 +3,7 @@ package com.inonu.stok_takip.dto.Request;
 import java.time.LocalDate;
 
 public record TicketSalesDetailCreateRequest(
+    //Deneme Amaclı
     int quantity,
     Double totalPrice,
     LocalDate ticketDate,
